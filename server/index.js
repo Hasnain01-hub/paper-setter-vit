7fbb7a52f462ef25b62f95f0ba230fd26d649d9f
Hasnain01-hub <hasnainsayyed833@gmail.com>
2023-03-08 21:56:07 +0530

    server fixes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ mongoose
     {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
     }
   )
@@ -25,15 +24,17 @@ mongoose
 
 //middleware
 app.use(morgan("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "2mb" }));
-app.use(morgan("dev"));
+app.use(morgan("common"));
 app.use(cors());
 
 //routes midleware
-readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 app.use("/", function (req, res) {
   res.send("Hello World");
 });
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 //port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
